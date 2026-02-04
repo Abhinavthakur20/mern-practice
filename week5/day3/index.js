47a60e85e4ac9d0b17d64b364f245269ba124e92
Abhinav Thakur <thakurabhinav16160@gmail.com>
2026-02-04 20:46:06 +0530

    basics js

---
+++ week5/day3/index.js
@@ -0,0 +1,14 @@
+var person_name= "Abhinav Thakur"
+console.log(person_name);
+
+var num1=50
+var num2 =25
+console.log("the addition of",num1 , "and" , num2 ,"is-", num1+num2);
+
+console.log("num1 + num2 =",num1+num2);
+console.log("num1 - num2 =",num1-num2);
+console.log("num1 * num2 =",num1*num2);
+console.log("num1 / num2 =",num1/num2);
+
+
+
